d6e441006f9a7d0f6e4bcbf66d99afb4640bef69
Bernat <bernatgarciacarbo@gmail.com>
2025-02-13 09:50:37 +0100

    wip

--- doordeck-sdk/build.gradle.kts
+++ doordeck-sdk/build.gradle.kts
@@ -408,10 +408,8 @@ classifiers = [
 "Homepage" = "${pypiPublish.authorHomepage}"
 "Source" = "${pypiPublish.gitRepository}"
 "Issue tracker" = "${pypiPublish.issues}"
-[tool.setuptools.packages.find]
-where = ["${pypiPublish.packageName}"]
-[tool.setuptools.package-data]
-${pypiPublish.packageName} = ["_doordeck_headless_sdk.pyd", "Doordeck.Headless.Sdk.dll"]
+[tool.setuptools]
+package-data = { "doordeck_headless_sdk" = ["_doordeck_headless_sdk.pyd", "Doordeck.Headless.Sdk.dll"] }
 """.trimIndent()
 
 tasks.register("pythonPack").configure {
